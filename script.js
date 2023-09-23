e898cbfac267747bf13cb0bd0314ceb47969dfd7
Whirlwindraven <fadysherif1998@gmail.com>
2023-09-23 12:54:33 -0400

    added listeners and functions

--- script.js
+++ script.js
@@ -1,7 +1,90 @@
-const imputContainer = document.getElementById('input-container');
+const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownform');
 const dateEl = document.getElementById('date-picker');
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const timeElements = document.querySelectorAll('span')
 
+
+let countdownTitle = '';
+let countdownDate = '';
+let countdownValue = Date;
+let countdownActive;
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+const completeEl = document.getElementById('complete');
+const completeElInfo = document.getElementById('complete-info');
+const completeBtn = document.getElementById('complete-button');
+//populate countdown
+function updateDom(){
+    countdownActive = setInterval(() =>{
+        const now = new Date().getTime();
+    const distance = countdownValue - now;
+
+    const days = Math.floor(distance / day);
+    const hours =  Math.floor((distance % day) / hour);
+    const minutes =  Math.floor((distance % hour) / minute);
+    const seconds =  Math.floor((distance % minute) / second);
+
+     // hide input 
+     inputContainer.hidden = true;
+
+     //if countdown ended show complete
+     if(distance <0){
+        countdownEl.hidden = true;
+        completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
+        completeEl.hidden = false;
+     } else{
+            // populating countdown
+    countdownElTitle.textContent = `${countdownTitle}`;
+    timeElements[0].textContent = `${days}`;
+    timeElements[1].textContent = `${hours}`;
+    timeElements[2].textContent = `${minutes}`;
+    timeElements[3].textContent = `${seconds}`;
+    completeEl.hidden = true;
+    countdownEl.hidden = false;
+     }
+    }, second);
+}
 // set date input min
 const today = new Date().toISOString().split('T');[0];
 dateEl.setAttribute('min', today); 
+
+
+// take values from form 
+function updateCountdown(e){
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[0].value;
+
+  // check for valid date
+  if(countdownDate === ''){
+    alert('Please select a date');
+  } else{
+      // get number of current date
+      countdownValue = new Date(countdownDate).getTime();
+      updateDom();
+  }
+}
+
+// reset all values
+function reset(){
+    countdownEl.hidden = true;
+    completeEl.hidden = true;
+    inputContainer.hidden = false;
+  
+    clearInterval(countdownActive);
+
+    countdownTitle = '';
+    countdownDate = '';
+}
+
+
+// event listner
+countdownForm.addEventListener('submit', updateCountdown);
+countdownBtn.addEventListener('click', reset);
+completeBtn.addEventListener('click', reset);
